8db08053f2be619a3a48b3ffbd0f3f681ef52be3
Koye <54333700+kryptcode@users.noreply.github.com>
2022-07-29 19:17:34 +0100

    Hero Section

---
+++ components/Hero.js
@@ -0,0 +1,38 @@
+import Typewriter from 'typewriter-effect';
+
+const Hero = () => {
+  return (
+    <div className='md:w-[90%] mx-auto py-32 px-4 md:0'>
+        <div className='text-4xl font-bold mb-14'>
+            Hello, I'm Koye
+        </div>
+        <div className='text-3xl text-[#000aff] font-medium mb-9'>
+            <Typewriter
+                options={{
+                    strings: ["Front End Developer", "Javascript Developer", "React Developer", "Web Developer"],
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
+        </div>
+
+        <div className='space-x-8 flex-wrap cursor-pointer mb-8'>
+            <span className='text-5xl'>
+                <i className="fa-brands fa-github hover:text-[#161b22] hover:scale-105 transition-all duration-200"></i>
+            </span>
+            <span className='text-5xl'>
+                <i class="fa-brands fa-linkedin hover:text-blue-900 hover:scale-105 transition-all duration-200 "></i>
+            </span>
+            <span className='text-5xl'>
+                <i class="fa-brands fa-twitter hover:text-blue-700 hover:scale-105 transition-all duration-200 "></i>
+            </span>
+        </div>
+
+        <button className='border-[3px]  border-black hover:bg-black hover:text-white px-4 py-2 text-xl font-medium transition-all duration-200 ease-linear'>
+            Get in touch
+        </button>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/Nav.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+const styles = {
+  button: 'cursor-pointer hover:underline',
+  container: 'md:w-[90%] mx-auto flex justify-between items-end py-6 px-4 md:px-0',
+  navContainer: 'space-x-6 uppercase hidden md:inline-block font-medium'
+}
+
+const Nav = () => {
+  return (
+    <div className={styles.container}>
+      <div className="text-3xl font-bold text-[#000aff]">
+        KA.
+      </div>
+      <div className={styles.navContainer}>
+        <Link href='/'>
+        <span className={styles.button}>home</span>
+        </Link>
+        <Link href='#about'>
+        <span className={styles.button}>about me</span>
+        </Link>
+        <Link href='#projects'>
+        <span className={styles.button}>projects</span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
