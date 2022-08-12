7579fc8417b40a7c42ca55900432cd1d76102d36
Koye <54333700+kryptcode@users.noreply.github.com>
2022-08-12 22:23:10 +0100

    visual bugs fixed - technologies and ctas
    Technologies and CTAs size and spacing optimized for smaller screens.

--- components/Project.js
+++ components/Project.js
@@ -2,9 +2,12 @@ const styles = {
     container: 'flex flex-col-reverse space-y-10 md:space-y-0 md:flex-row justify-evenly z-2 mb-32 shadow-md shadow-black/10',
     textWrapper: 'py-5 md:py-10 px-8 flex flex-col justify-between flex-1',
     heading: 'text-4xl md:text-6xl font-bold mb-8',
+    description: 'text-lg font-medium mb-6',
+    techsContainer: 'text-md flex flex-wrap gap-x-4 mb-8',
+    techs: 'bg-white text-sm md:text-md p-1 md:p-2',
     buttonsWrapper: 'space-x-6 text-lg mb-2 font-semibold',
-    liveBtn: 'py-2 px-8 border border-black bg-black text-white',
-    codeBtn: 'py-2 px-8 border border-black',
+    liveBtn: 'py-2 px-4 md:px-8 border border-black bg-black text-white',
+    codeBtn: 'py-2 px-4 md:px-8 border border-black',
     imageContainer: 'bg-[#f8fafc] flex items-center justify-center flex-1',
     image: 'md:w-[95%] md:rounded-md shadow-xl shadow-black/40'
 }
@@ -16,13 +19,13 @@ const Project = ({ project }) => {
             <h2 className={styles.heading}>
                     {project.name}
             </h2>
-            <p className='text-lg font-medium mb-6'>
+            <p className={styles.description}>
                   {project.description}
             </p>
-            <div className="text-md flex flex-wrap space-x-4 mb-8 break-words">
+            <div className={styles.techsContainer}>
                 {
                     project.techs.map(tech => (
-                        <span className="bg-white p-2">
+                        <span className={styles.techs} key={tech.id}>
                             {tech}
                         </span>
                     ))
